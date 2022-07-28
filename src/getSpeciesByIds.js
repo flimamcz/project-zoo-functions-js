6f90e60112cb34b2eed47bbfdbd522d7ab6da964
Filipe Lima <cimofelipe4@gmail.com>
2022-07-28 20:51:25 -0300

    Refatorando requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -3,8 +3,8 @@ const data = require('../data/zoo_data');
 const { species } = data;
 
 function getSpeciesByIds(...ids) {
-  const filterSpecies = ids.map((id) => species.find((element) => element.id === id));
-  return filterSpecies;
+  return ids.map((id) => species.find((element) => element.id === id));
 }
+getSpeciesByIds('533bebf3-6bbe-41d8-9cdf-46f7d13b62ae', 'e8481c1d-42ea-4610-8e11-1752cfc05a46');
 
 module.exports = getSpeciesByIds;
