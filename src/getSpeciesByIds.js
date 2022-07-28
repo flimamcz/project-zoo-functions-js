dd237f089820a05b9d349a476b56688b29bebd99
Filipe Lima <cimofelipe4@gmail.com>
2022-07-28 19:55:25 -0300

    Refatorado requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -3,17 +3,8 @@ const data = require('../data/zoo_data');
 const { species } = data;
 
 function getSpeciesByIds(...ids) {
-  if (ids.length === 0) return ids;
-
-  const arrayIndex = [];
-  const getSpecies = species.map((element) => element.id);
-
-  getSpecies.filter((id, index) => arrayIndex.push(getSpecies.indexOf(ids[index])));
-
-  const arrayWin = arrayIndex
-    .filter((element) => element >= 0 && element <= getSpecies.length);
-
-  return arrayWin.map((element) => species[element]);
+  const filterSpecies = ids.map((id) => species.find((element) => element.id === id));
+  return filterSpecies;
 }
 
 module.exports = getSpeciesByIds;
