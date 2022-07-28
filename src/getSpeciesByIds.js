140238124bef275d69edf186b6a6f0424f9b6693
Filipe Lima <cimofelipe4@gmail.com>
2022-07-28 19:35:26 -0300

    Requisito 1 finalizado

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,19 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getSpeciesByIds(...ids) {
   if (ids.length === 0) return ids;
+
+  const arrayIndex = [];
+  const getSpecies = species.map((element) => element.id);
+
+  getSpecies.filter((id, index) => arrayIndex.push(getSpecies.indexOf(ids[index])));
+
+  const arrayWin = arrayIndex
+    .filter((element) => element >= 0 && element <= getSpecies.length);
+
+  return arrayWin.map((element) => species[element]);
 }
 
-getSpeciesByIds('533bebf3-6bbe-41d8-9cdf-46f7d13b62ae');
 module.exports = getSpeciesByIds;
