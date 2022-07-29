b4fc16f6a2904bc98c35914ae7b11dec8d4a7e5a
Filipe Lima <cimofelipe4@gmail.com>
2022-07-28 22:01:57 -0300

    Requisito 2 finalizado

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animalThanMin = species
+    .filter((element) => element.name === animal)
+    .map((item) => item.residents)
+    .every((it, index) => it[index].age > age);
+  return animalThanMin;
 }
 
 module.exports = getAnimalsOlderThan;
