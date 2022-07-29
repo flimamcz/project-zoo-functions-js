393d27943d0d5d074f68570bc9339e00015ecfd1
Filipe Lima <cimofelipe4@gmail.com>
2022-07-29 18:08:56 -0300

    Requisito 4 finalizado: obs: refatorar

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -12,24 +12,23 @@ function isManager(id) {
 
 function getRelatedEmployees(managerId) {
   const managerVerify = isManager(managerId);
-  if (!managerVerify) throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  const manager = employees.find((person) => person.id === managerId);
+  if (!managerVerify) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+
   try {
-    if (managerVerify) {
-      const subordinates = [];
-      const manager = employees.find((person) => person.id === managerId);
-      employees.forEach((people, index) => {
-        people.managers.find((id) => (
-          id === manager.id ? subordinates.push(`${people.firstName} ${people.lastName}`) : null
-        ));
-      });
-      return subordinates;
-    }
+    const subordinates = [];
+    employees.forEach((people, index) => {
+      people.managers.find((id) => (
+        id === manager.id ? subordinates.push(`${people.firstName} ${people.lastName}`) : null
+      ));
+    });
+    return subordinates;
   } catch (error) {
-    return error.message;
+    return error;
   }
 }
-
-console.log(getRelatedEmployees('b0dc644a-5335-489b-8a2c-4e086c7819a2'));
 
 module.exports = { isManager, getRelatedEmployees };
 
@@ -39,3 +38,11 @@ module.exports = { isManager, getRelatedEmployees };
 //   }
 // });
 // })
+
+// if (managerVerify) {
+//   employees.map((employee) => employee)
+//   .filter((element) => { element.managers.forEach((id) =>id === manager.id ? sub.push(`${element.firstName} ${element.lastName}`) : null);
+//   });
+
+//   return sub;
+// }
