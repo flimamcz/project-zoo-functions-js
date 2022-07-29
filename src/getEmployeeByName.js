21392b8cd4d6f742db1a2be3d24b4d7832f85eed
Filipe Lima <cimofelipe4@gmail.com>
2022-07-29 09:22:27 -0300

    Requisito 3 finalizado

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) return {};
+  const getEmployee = employees.filter((employee) => (
+    employee.firstName === employeeName || employee.lastName === employeeName
+  ));
+
+  return getEmployee[0];
 }
 
 module.exports = getEmployeeByName;
