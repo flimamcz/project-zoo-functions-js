1011c72745f6b2f9a80b4cf840f6b0c310c17df9
Filipe Lima <cimofelipe4@gmail.com>
2022-07-30 13:00:04 -0300

    Requisito 5 finalizado

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,44 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  const namesAnimals = species.map((specie) => specie.name);
+  const valuesAnimals = species.map((specie) => Object.keys(specie.residents).length);
+  const createObjAnimals = namesAnimals.map((name, index) => {
+    const animals = {};
+    animals[`${name}`] = valuesAnimals[index];
+    return animals;
+  });
+  const animalObject = Object.assign({}, ...createObjAnimals);
+  if (animal === undefined) {
+    return animalObject;
+  }
+  const lengthParamenter = Object.keys(animal).length;
+  const getAnimalByName = species.find((specie) => specie.name === animal.specie).residents.length;
+  const getAnimalBySexeName = species
+    .find((specie) => specie.name === animal.specie)
+    .residents.filter((element) => element.sex === animal.sex).length;
+  if (lengthParamenter === 1) return getAnimalByName;
+  return getAnimalBySexeName;
 }
 
+console.log(countAnimals({ specie: 'bears', sex: 'female' }));
+
 module.exports = countAnimals;
+
+// const count = species.map((element) => (
+//   Object.keys(element.residents).length));
+// if (animal === undefined) {
+//   return {
+//     lions: count[0],
+//     tigers: count[1],
+//     bears: count[2],
+//     penguins: count[3],
+//     otters: count[4],
+//     frogs: count[5],
+//     snakes: count[6],
+//     elephants: count[7],
+//     giraffes: count[8],
+//   };
+// }
