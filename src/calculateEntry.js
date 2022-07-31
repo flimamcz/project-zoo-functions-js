80123ae36ba297f00d2e8c4a2aa185910b387cb2
Filipe Lima <cimofelipe4@gmail.com>
2022-07-31 10:13:34 -0300

    Requisito 8 finalizado

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,37 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
 function countEntrants(entrants) {
-  // seu código aqui
+  let child = 0;
+  let adult = 0;
+  let senior = 0;
+  const ages = entrants.map((people) => people.age);
+  ages.forEach((element) => {
+    if (element < 18) {
+      child += 1;
+    } else if (element >= 18 && element < 50) {
+      adult += 1;
+    } else {
+      senior += 1;
+    }
+  });
+  return {
+    child,
+    adult,
+    senior,
+  };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const peoplesAge = countEntrants(entrants);
+  const valueTotal = ((prices.child * peoplesAge.child)
+    + (prices.adult * peoplesAge.adult)
+    + (prices.senior * peoplesAge.senior));
+  return valueTotal;
 }
 
 module.exports = { calculateEntry, countEntrants };
