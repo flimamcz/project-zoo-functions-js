0e9867ea6eb00427db96691e2c21bc93ae029b1e
Filipe Lima <cimofelipe4@gmail.com>
2022-08-01 12:31:04 -0300

    requisito 10: refatorando funções (divisao)

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -7,17 +7,27 @@ function getEmployeeById(id) {
     .find((employee) => employee.id === id)
     .responsibleFor.map((identify) => (
       species.filter((specie) => specie.id === identify)))[0];
+  return peopleEmployeeAnimals;
+}
 
-  const getAges = peopleEmployeeAnimals[0].residents.map((element) => element.age);
+function getOldestFromFirstSpecies(id) {
+  const animalSelectByPerson = getEmployeeById(id);
+  const getAges = animalSelectByPerson[0].residents.map((element) => element.age);
   const olderAge = Math.max(...getAges);
-  const olderAgeAnimal = peopleEmployeeAnimals[0]
+
+  const olderAgeAnimal = animalSelectByPerson[0]
     .residents.find((animal) => animal.age === olderAge);
 
   return Object.values(olderAgeAnimal);
 }
 
-function getOldestFromFirstSpecies(id) {
-  return getEmployeeById(id);
-}
+console.log((getOldestFromFirstSpecies('56d43ba3-a5a7-40f6-8dd7-cbb05082383f')));
 
 module.exports = getOldestFromFirstSpecies;
+
+// const getAges = peopleEmployeeAnimals[0].residents.map((element) => element.age);
+// const olderAge = Math.max(...getAges);
+// const olderAgeAnimal = peopleEmployeeAnimals[0]
+//   .residents.find((animal) => animal.age === olderAge);
+
+// return Object.values(olderAgeAnimal);
