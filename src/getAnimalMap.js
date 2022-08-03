03c9ec0507031c474223cf1a3f3af1731fc66452
Filipe Lima <cimofelipe4@gmail.com>
2022-08-03 13:21:51 -0300

    Feito 3 primeiros requisitos do teste

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,35 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+const animalsLocations = {
+  NE: species.filter((specie) => specie.location === 'NE'),
+  NW: species.filter((specie) => specie.location === 'NW'),
+  SE: species.filter((specie) => specie.location === 'SE'),
+  SW: species.filter((specie) => specie.location === 'SW'),
+};
+
+const filteredAnimals = () => {
+  const locations = Object.keys(animalsLocations);
+  const filterAnimals = Object.entries(animalsLocations)
+    .map((animal) => animal[1])
+    .map((element) => element.map((item) => item.name));
+
+  const arrayAnimalsAndLocation = locations
+    .map((location, index) => [location, filterAnimals[index]]);
+
+  const objAnimals = Object.fromEntries(arrayAnimalsAndLocation);
+  return objAnimals;
+};
+
+console.log(filteredAnimals());
+
 function getAnimalMap(options) {
-  // seu código aqui
+  if (options === undefined) return filteredAnimals();
+  const { includeNames, sex, sorted } = options;
+  if (!includeNames) return filteredAnimals();
 }
+
+getAnimalMap({ name: 1 });
 
 module.exports = getAnimalMap;
